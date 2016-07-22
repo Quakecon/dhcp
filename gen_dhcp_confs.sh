ff9d3d17c46a0ea129b6f4c7c1259e73b02c0c03
Shawn Nock <nock@nocko.se>
2016-07-22 15:48:53 -0400

    Initial commit

---
+++ gen_dhcp_confs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SECOND_OCTET=$1
+START_RANGE=$2
+END_RANGE=$3
+
+for THIRD_OCTET in `seq $START_RANGE $END_RANGE`
+do
+  IPX="172.$SECOND_OCTET.$THIRD_OCTET"
+  echo "subnet $IPX.0 netmask 255.255.255.0 {"
+  echo "  option subnet-mask 255.255.255.0;"
+  echo "  option routers $IPX.1;"
+  echo "  pool {"
+  echo "    failover peer \"dhcp-failover\";"
+  echo "    range $IPX.130 $IPX.254;"
+  echo "  }"
+  echo "}"
+  echo ""
+done
